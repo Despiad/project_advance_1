30d5c397416875eae7b06381b9ac561cf6aa1353
balikfromua <yuhimenko.valentin@gmail.com>
2019-01-06 20:25:18 +0200

    menu bar added

--- application/src/main/java/advanced/balik/application/view/RootLayoutController.java
+++ application/src/main/java/advanced/balik/application/view/RootLayoutController.java
@@ -1,6 +1,7 @@
 package advanced.balik.application.view;
 
 import advanced.balik.application.MainApp;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 
 public class RootLayoutController {
@@ -13,5 +14,13 @@ public class RootLayoutController {
 
     public void setMainApp(MainApp mainApp) {
         this.mainApp=mainApp;
+    }
+
+    /**
+     * Завершить работу программы.
+     */
+    @FXML
+    public void close() {
+        Platform.exit();
     }
 }
